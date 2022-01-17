af49facd63ecca4c25fb08604ce90f97a6834044
ItaloLA <lima_italo_amaral@hotmail.com>
2022-01-16 21:18:56 -0300

    traduzir condicao

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
 
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
+
 gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671);
 
@@ -22,7 +32,7 @@ pegarPersonagem = () => {
         imagem.alt = data.name;
         nomeDoPersonagem.innerHTML = data.name;
         especie.innerHTML = data.especies;
-        condicao.innerHTML = data.status;
+        condicao.innerHTML = traduzirCondicao(data);
     });
 }
 
